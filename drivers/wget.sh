5f902fac43b318653a99eb3386fe01d00d9ccecd
shabane <m.mohamadshabane@gmail.com>
2022-07-14 12:32:52 +0430

    [fix](remove -k from wget driver)

--- drivers/wget.sh
+++ drivers/wget.sh
@@ -1,7 +1,7 @@
 while true
 do	
 	mkdir /tmp/dw
-	cd /tmp/dw && wget --wait 0.5 --random-wait -mkp $URL
+	cd /tmp/dw && wget --wait 0.5 --random-wait -mp $URL
 	rm /tmp/dw -rf
 	let COUNTER=COUNTER+1
 	echo 'Done! Let Start Over'
